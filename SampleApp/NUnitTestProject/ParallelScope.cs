adaefc087c6b504d44d3c7b372ce137149aa5546
Irfan Pasha Syed <isyed@progress.com>
2020-06-28 14:13:38 -0400

    Add comments

--- SampleApp/NUnitTestProject/ParallelScope.cs
+++ SampleApp/NUnitTestProject/ParallelScope.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
 
+// If we create a .cs file with the following parameters then .NET reads these assembly properties and set parallelism
 [assembly: Parallelizable(ParallelScope.All)]
 [assembly: LevelOfParallelism(20)]
